b5c5587beb16faa397356243bcd58aae638125c2
StarryPinnacle <huangfustar@qq.com>
2026-06-05 22:59:16 +0800

    ✨feat(klib/string): implement partial string function
    - pass the string test in am-kernel test

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,11 +5,17 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  size_t idx = 0;
+  while (s[idx]) idx++;
+  return idx;
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  size_t i;
+
+  for (i = 0; src[i]; i++) dst[i] = src[i];
+  dst[i] = '\0';
+  return dst;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
@@ -17,11 +23,17 @@ char *strncpy(char *dst, const char *src, size_t n) {
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  size_t dst_len = strlen(dst);
+  size_t i = 0;
+  for (; src[i]; i++) dst[dst_len + i] = src[i];
+  return dst;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  int idx = 0;
+  while (s1[idx] && s2[idx] && s1[idx] == s2[idx]) idx++;
+  if (!s1[idx] && !s2[idx]) return 0;
+  else return s1[idx] - s2[idx];
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
@@ -29,7 +41,8 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  for (size_t i = 0; i < n; i++) ((unsigned char *)s)[i] = c;
+  return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
@@ -41,7 +54,11 @@ void *memcpy(void *out, const void *in, size_t n) {
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  if (n == 0) return 0;
+  size_t i;
+  for (i = 0; i < n && ((unsigned char *) s1)[i] == ((unsigned char *) s2)[i]; i++);
+  if (i == n) return 0;
+  return (((unsigned char *) s1)[i]) - (((unsigned char *) s2)[i]);
 }
 
 #endif
