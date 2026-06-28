78ef8383130fb48d6917f403c7286b9de525036d
StarryPinnacle <huangfustar@qq.com>
2026-06-28 11:53:29 +0800

    feat(klib/stdlib): implement malloc

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -4,6 +4,7 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 static unsigned long int next = 1;
+static char *hbrk = NULL;
 
 int rand(void) {
   // RAND_MAX assumed to be 32767
@@ -34,7 +35,17 @@ void *malloc(size_t size) {
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  panic("Not implemented");
+  if (hbrk == NULL) {
+    hbrk = (char *)ROUNDUP(heap.start, 8);
+  }
+
+  size = (size_t)ROUNDUP(size, 8);
+  char *old = hbrk;
+  hbrk += size;
+
+  assert((uintptr_t)heap.start <= (uintptr_t)old);
+  assert((uintptr_t)hbrk <= (uintptr_t)heap.end);
+  return old;
 #endif
   return NULL;
 }
